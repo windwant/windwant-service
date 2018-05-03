b03377be3d2997ff0682dd7e207f995b56751ef0
junlei.nie <nie.junlei@hotmail.com>
2018-05-03 14:04:57 +0800

    format

--- nio-demo/src/main/java/org/windwant/nio/NIOClient.java
+++ nio-demo/src/main/java/org/windwant/nio/NIOClient.java
@@ -27,22 +27,16 @@ public class NIOClient {
 
     public static void main(String[] args) throws IOException {
         // 打开socket通道
-
         SocketChannel socketChannel = SocketChannel.open();
         // 设置为非阻塞方式
-
         socketChannel.configureBlocking(false);
         // 打开选择器
-
         Selector selector = Selector.open();
         // 注册连接服务端socket动作
-
         socketChannel.register(selector, SelectionKey.OP_CONNECT);
         // 连接
-
         socketChannel.connect(SERVER_ADDRESS);
         // 分配缓冲区大小内存
-
 
         Set<SelectionKey> selectionKeys;
         Iterator<SelectionKey> iterator;
@@ -50,16 +44,13 @@ public class NIOClient {
         SocketChannel client;
         String receiveText;
         String sendText;
-        int count=0;
+        int count = 0;
 
         while (true) {
             //选择一组键，其相应的通道已为 I/O 操作准备就绪。
-
             //此方法执行处于阻塞模式的选择操作。
-
             selector.select();
             //返回此选择器的已选择键集。
-
             selectionKeys = selector.selectedKeys();
             //System.out.println(selectionKeys.size());
 
@@ -72,7 +63,6 @@ public class NIOClient {
                     // 判断此通道上是否正在进行连接操作。
 
                     // 完成套接字通道的连接过程。
-
                     if (client.isConnectionPending()) {
                         client.finishConnect();
                         System.out.println("完成连接!");
@@ -84,15 +74,15 @@ public class NIOClient {
                     client.register(selector, SelectionKey.OP_READ);
                 } else if (selectionKey.isReadable()) {
                     client = (SocketChannel) selectionKey.channel();
+
                     //将缓冲区清空以备下次读取
-
                     receivebuffer.clear();
-                    //读取服务器发送来的数据到缓冲区中
 
-                    count=client.read(receivebuffer);
-                    if(count>0){
-                        receiveText = new String( receivebuffer.array(),0,count);
-                        System.out.println("客户端接受服务器端数据--:"+receiveText);
+                    //读取服务器发送来的数据到缓冲区中
+                    count = client.read(receivebuffer);
+                    if (count > 0) {
+                        receiveText = new String(receivebuffer.array(), 0, count);
+                        System.out.println("客户端接受服务器端数据--:" + receiveText);
                         client.register(selector, SelectionKey.OP_WRITE);
                     }
 
@@ -101,11 +91,11 @@ public class NIOClient {
                     client = (SocketChannel) selectionKey.channel();
                     sendText = "message from client--" + (flag++);
                     sendbuffer.put(sendText.getBytes());
-                    //将缓冲区各标志复位,因为向里面put了数据标志被改变要想从中读取数据发向服务器,就要复位
 
+                    //将缓冲区各标志复位,因为向里面put了数据标志被改变要想从中读取数据发向服务器,就要复位
                     sendbuffer.flip();
                     client.write(sendbuffer);
-                    System.out.println("客户端向服务器端发送数据--："+sendText);
+                    System.out.println("客户端向服务器端发送数据--：" + sendText);
                     client.register(selector, SelectionKey.OP_READ);
                 }
             }
