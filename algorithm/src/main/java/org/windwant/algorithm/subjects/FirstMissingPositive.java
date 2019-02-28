9334a779f93295e54e3fa7a6221906142126be8e
windwant <nie.junlei@hotmail.com>
2019-02-28 15:21:36 +0800

    数组，找出其中没有出现的最小的正整数

---
+++ algorithm/src/main/java/org/windwant/algorithm/subjects/FirstMissingPositive.java
@@ -0,0 +1,65 @@
+package org.windwant.algorithm.subjects;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个未排序的整数数组，找出其中没有出现的最小的正整数。
+ * 你的算法的时间复杂度应为O(n)，并且只能使用常数级别的空间。
+ *
+ * Created by Administrator on 19-2-28.
+ */
+public class FirstMissingPositive {
+
+    public static int firstMissingPositive(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] < 0) continue;
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+
+        max = max == Integer.MAX_VALUE?max:max + 2;
+        for (int i = 1; i < max; i++) {
+            if(contains(nums, i)) continue;
+            return i;
+        }
+        return max + 1;
+    }
+
+    public static boolean contains(int[] nums, int ele){
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == ele) return true;
+        }
+        return false;
+    }
+
+    public static int firstMissingPositive1(int[] nums) {
+        Map<Integer, Integer> vs = new HashMap<>();
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] < 0) continue;
+            if(nums[i] > max){
+                max = nums[i];
+            }
+            vs.put(nums[i], i);
+        }
+
+        max = max == Integer.MAX_VALUE?max:max + 2;
+        for (int i = 1; i < max; i++) {
+            if(vs.get(i) != null) continue;
+            return i;
+        }
+        return max + 1;
+    }
+
+    public static void main(String[] args) {
+//        int[] nums = {1,2,0};
+//        int[] nums = {3,4,-1,1};
+//        int[] nums = {7,8,9,11,12};
+        int[] nums = {2147483647};
+        System.out.println(firstMissingPositive(nums));
+    }
+}
