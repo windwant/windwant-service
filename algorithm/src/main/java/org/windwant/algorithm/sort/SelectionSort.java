15759bdd04cc5b1bfa51369306850286e8644563
windwant <nie.junlei@hotmail.com>
2019-03-11 11:23:57 +0800

    选择排序更正

--- algorithm/src/main/java/org/windwant/algorithm/sort/SelectionSort.java
+++ algorithm/src/main/java/org/windwant/algorithm/sort/SelectionSort.java
@@ -5,18 +5,42 @@ import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * 每次找出剩下集合中最小的值 交换位置
+ * 
+ * 降序和升序
  * Created by windwant on 2016/12/5.
  */
 public class SelectionSort {
 
-    public static void SelectionSort(int[] arr){
+    public static void SelectionSortAsc(int[] arr){
+        int min = 0;
         for (int i = 0; i < arr.length - 1; i++) {
+            min = i;
             for (int j = i + 1; j < arr.length ; j++) {
-                if(arr[j] < arr[i]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                if(arr[j] < arr[min]){
+                    min = j;
                 }
+            }
+            if(min != i){
+                int tmp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = tmp;
+            }
+        }
+    }
+
+    public static void SelectionSortDesc(int[] arr){
+        int max = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            max = i;
+            for (int j = i + 1; j < arr.length ; j++) {
+                if(arr[j] > arr[max]){
+                    max = j;
+                }
+            }
+            if(max != i){
+                int tmp = arr[i];
+                arr[i] = arr[max];
+                arr[max] = tmp;
             }
         }
     }
@@ -28,7 +52,9 @@ public class SelectionSort {
             arr[i] = ThreadLocalRandom.current().nextInt(100);
         }
         System.out.println("before: " + Arrays.toString(arr));
-        SelectionSort(arr);
-        System.out.println(" after: " + Arrays.toString(arr));
+        SelectionSortDesc(arr);
+        System.out.println("DESC after: " + Arrays.toString(arr));
+        SelectionSortAsc(arr);
+        System.out.println("ASC after: " + Arrays.toString(arr));
     }
 }
